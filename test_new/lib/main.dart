ac9059ea557cf4ab5e289ba01cb8db497abc7c4d
A00A00A 0 <1299233936@qq.com>
2024-05-07 08:01:38 +0000

    main

--- test_new/lib/main.dart
+++ test_new/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'test_new',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
